c4226bceb13016029dd158a36db8d1b2b755eed1
Seth Poophak <42564156+SPRYK@users.noreply.github.com>
2020-11-15 04:12:23 +0700

    add post management

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+from django.urls import reverse
+
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
@@ -10,3 +11,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
+
+    
